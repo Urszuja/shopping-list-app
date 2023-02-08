65ad68de371435da09220c885bf185e44f28b94b
Urszuja <urszula.jarych@capgemini.com>
2023-02-08 10:47:21 +0100

    recipe service added

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,23 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipesService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Omelette du fromage',
+      'omlette with cottage cheese',
+      'https://live.staticflickr.com/4198/34816100336_4a4a79baae_c.jpg'
+    ),
+  ];
+
+  constructor() {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
